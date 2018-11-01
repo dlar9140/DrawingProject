019b94dfafab61d4e30ed8518d8edae76caa4c5e
David <davidlarson46@yahoo.com>
2018-11-01 09:01:10 -0600

    connected the UIBezierPath to the drawingView

--- DrawingProject/View/DrawingView.swift
+++ DrawingProject/View/DrawingView.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
-class DrawingView: UIView {
+public class DrawingView: UIView {
 
-    /*
+    
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+   public override func draw(_ rect: CGRect) -> Void
+   {
+    // Drawing code
     }
-    */
+    
+    private func createStickFigure() -> UIBezierPath
+    {
+        let figure : UIBezierPath = UIBezierPath()
+        
+        
+        return figure 
+    }
 
 }
